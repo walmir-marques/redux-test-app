7b49c4244c7520c744ac12c4e9b4f7c53709af1c
Walmir Marques <walmir-mf@hotmail.com>
2024-01-24 14:35:09 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setName, setAge } from "./redux/reducers/userReducer";
+import { setTheme } from "./redux/reducers/themeReducer";
 
 function App() {
+  const user = useSelector((state) => state.user);
+  const theme = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
+
+  const handleNameInput = (e) => {
+    dispatch(setName(e.target.value));
+  };
+
+  const handleAgeInput = (e) => {
+    dispatch(setAge(parseInt(e.target.value)));
+  };
+
+  const handleChangeTheme = () => {
+    dispatch(setTheme(theme.status === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Meu nome é: {user.name} e tenho {user.age} anos. Tema: {theme.status}
+      <hr />
+      <input type="text" value={user.name} onChange={handleNameInput} />
+      <input type="text" value={user.age} onChange={handleAgeInput} />
+      <button onClick={handleChangeTheme}>Switch Theme</button>
     </div>
   );
 }
 
 export default App;
+
+//passos para criar o redux -store, provider, reducers / slice , actions / dispatches, selectors
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./reducers/userReducer";
+import themeReducer from "./reducers/themeReducer";
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    theme: themeReducer,
+  },
+});
